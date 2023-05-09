43c7ce7da7287b25e497748dd0c939810960a466
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-05-09 17:45:09 +0530

    Get current location weather and added toast to app

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
+import { toast } from 'react-toastify';
 
-const Inputs = () => {
+const Inputs = ({setQuery, units,setUnits}) => {
+  const [city, setCity] = useState('');
+
+  const handleUnitsChange = (e) =>{
+    const selectedunit = e.currentTarget.name
+    if(units !== selectedunit) setUnits(selectedunit)
+  }
+
+const handleSearchClick = () =>{
+  if(city !== '') setQuery({q: city})
+}
+
+const handleLocationClick = () =>{
+  if(navigator.geolocation ){
+    toast.info('Fetching users location')
+    navigator.geolocation.getCurrentPosition((position)=>{
+      toast.info('Location fetched')
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+
+      setQuery({
+        lat,
+        lon
+      })
+    })
+  }
+}
+
   return (
     <div className='flex flex-row justify-center my-6'>
         <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
-            <input type='text' placeholder='Search for city..' className='text-xl font-light p-2 w-full shadow-xl 
+            <input value={city} onChange={(e) => setCity(e.currentTarget.value)} type='text' placeholder='Search for city..' className='text-xl font-light p-2 w-full shadow-xl 
             focus:outline-none capitalize placeholder:lowercase'/>
-            <UilSearch size={25} className='text-white cursor-pointer transition ease-out hover:scale-125'/>
-            <UilLocationPoint size={25} className='text-white cursor-pointer transition ease-out hover:scale-125'/>
+            <UilSearch onClick={handleSearchClick} size={25} className='text-white cursor-pointer transition ease-out hover:scale-125'/>
+            <UilLocationPoint onClick={handleLocationClick}  size={25} className='text-white cursor-pointer transition ease-out hover:scale-125'/>
         </div>
         <div className='flex flex-row w-1/4 items-center justify-center'>
-            <button name='metric' className='text-xl text-white font-light'>°C</button>
+            <button onClick={handleUnitsChange}
+            name='metric' className='text-xl text-white font-light transition ease-out hover:scale-125'>°C</button>
             <p className='text-xl text-white mx-1'>|</p>
-            <button name='imperial' className='text-xl text-white font-light'>°F</button>
+            <button onClick={handleUnitsChange} name='imperial' className='text-xl text-white font-light transition ease-out hover:scale-125'>°F</button>
         </div>
     </div>
   )
--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TopButtons = () => {
+const TopButtons = ({setQuery}) => {
 
     const cities = [
         {
@@ -27,7 +27,9 @@ const TopButtons = () => {
   return (
     <div className='flex items-center justify-around my-6'>
         {cities.map((city) =>(
-            <button key={city.id} className='text-white text-lg font-medium'>{city.title}</button>
+            <button key={city.id} className='text-white text-lg font-medium'
+            onClick={()=> setQuery({q: city.title})}>
+                {city.title}</button>
         ))}
     </div>
   )
