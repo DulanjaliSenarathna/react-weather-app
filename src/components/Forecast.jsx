1fc2a5188be12c874d9d37a402f1bafbc2a694d3
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-05-09 14:56:29 +0530

    Fetch data from API to UI

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { iconUrlFromCode } from '../services/weatherService'
 
-const Forecast = ({title}) => {
+const Forecast = ({title,items}) => {
   return (
     <div>
         <div className='flex items-center justify-start mt-6 '>
@@ -8,35 +9,14 @@ const Forecast = ({title}) => {
         </div>
         <hr className='my-2' />
         <div className='flex flex-grow items-center justify-between text-white'>
-            <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>04:30 PM</p>
-                <img src="https://openweathermap.org/img/wn/01d@2x.png" className='w-12 my-1' alt="" />
-                <p className='font-medium'>22°</p>
+            {items.map(item =>(
+                <div className='flex flex-col items-center justify-center'>
+                <p className='font-light text-sm'>{item.title}</p>
+                <img src={iconUrlFromCode(item.icon)} className='w-12 my-1' alt="" />
+                <p className='font-medium'>{`${item.temp.toFixed()}°`}</p>
             </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>04:30 PM</p>
-                <img src="https://openweathermap.org/img/wn/01d@2x.png" className='w-12 my-1' alt="" />
-                <p className='font-medium'>22°</p>
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>04:30 PM</p>
-                <img src="https://openweathermap.org/img/wn/01d@2x.png" className='w-12 my-1' alt="" />
-                <p className='font-medium'>22°</p>
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>04:30 PM</p>
-                <img src="https://openweathermap.org/img/wn/01d@2x.png" className='w-12 my-1' alt="" />
-                <p className='font-medium'>22°</p>
-            </div>
-
-            <div className='flex flex-col items-center justify-center'>
-                <p className='font-light text-sm'>04:30 PM</p>
-                <img src="https://openweathermap.org/img/wn/01d@2x.png" className='w-12 my-1' alt="" />
-                <p className='font-medium'>22°</p>
-            </div>
+            ))}
+            
         </div>
     </div>
   )
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -43,7 +43,7 @@ const formatForecastWeather = (data) =>{
     hourly = hourly.slice(1,6).map(d=>{
         return{
             title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
-            temp: d.temp.day,
+            temp: d.temp,
             icon: d.weather[0].icon
         }
     });
